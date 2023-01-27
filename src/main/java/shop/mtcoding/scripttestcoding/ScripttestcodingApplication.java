d0001e22557205ac7609090d70ab132ddf2b6d7c
kimalss22 <kimalss22@gmail.com>
2023-01-27 16:19:58 +0900

    프로젝트 초기화

---
+++ src/main/java/shop/mtcoding/scripttestcoding/ScripttestcodingApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.scripttestcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScripttestcodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScripttestcodingApplication.class, args);
+	}
+
+}
